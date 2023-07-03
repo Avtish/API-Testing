26f5bcf135d8b0e8d88d5f0947d6125f427f3576
Avtish <avtishkakadiya@gmail.com>
2023-07-03 21:59:01 +0530

    combine folder

--- API/cypress.config.js
+++ API/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://api.tmsandbox.co.nz",
         defaultCommandTimeout: 10000,
-        video: true,
+        video: false,
         screenshotOnRunFailure: true,
 
     setupNodeEvents(on, config) {
